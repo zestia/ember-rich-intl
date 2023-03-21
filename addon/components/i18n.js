b1977b7bb13bc82f89deb63b59ab52a19e9a3b9d
Josh Jackson <joshcharlesjackson@gmail.com>
2023-03-21 10:25:25 +0000

    Initial commit

---
+++ addon/components/i18n.js
@@ -0,0 +1,48 @@
+import Component from '@glimmer/component';
+import { compileHBS } from 'ember-repl';
+
+export default class I18nComponent extends Component {
+  get template() {
+    const parts = this._getPartsFromString(this.args.string);
+
+    let hbs = '';
+
+    parts.forEach((part) => {
+      if (typeof part === 'object') {
+        hbs += `{{yield "${part.content}" to="${part.name}"}}`;
+      } else {
+        hbs += part;
+      }
+    });
+
+    return compileHBS(hbs);
+  }
+
+  /**
+   * @param {string} string
+   *
+   * Given the following string: "Visit our <:link>documentation</:link>."
+   * Return: ["Visit our ", {name: "link", content: "documentation"}, "."]
+   */
+  _getPartsFromString(string) {
+    const regex = /(<:[\w'-]+>.*?<\/:[\w'-]+>)/g;
+
+    const parts = string.split(regex).map((part) => {
+      const matches = part.match(regex);
+
+      if (matches !== null) {
+        const blockName = part.match(/<:(.*?)>/)[1];
+        const blockContent = part.match(/<:[\w'-]+>(.*?)<\/:[\w'-]+>/)[1];
+
+        return {
+          name: blockName,
+          content: blockContent,
+        };
+      }
+
+      return part;
+    });
+
+    return parts;
+  }
+}
---
+++ app/components/i18n.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-i18n-component/components/i18n';
