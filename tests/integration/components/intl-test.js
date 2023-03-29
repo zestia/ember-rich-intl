0aebbfc7616671688e1ba83c099a6759a4459c27
Andrew Kirwin <github@andrewkirwin.co.uk>
2023-03-29 12:45:07 +0100

    clean up test

--- tests/integration/components/intl-test.js
+++ tests/integration/components/intl-test.js
@@ -6,20 +6,23 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | intl', function (hooks) {
   setupRenderingTest(hooks);
 
-  hooks.beforeEach(function () {
-    this.string = `
-      Hello, this is a <Button>test string</Button> that can be
-      <Link>translated</Link> into multiple languages.
-      Inside it there are tokens <Icon />, which are swapped for
-      components allowing for dynamic content.
-    `;
-  });
-
   test('it works', async function (assert) {
     assert.expect(1);
 
+    this.input =
+      'Hello, this is a <Button>test string</Button> that can be ' +
+      '<Link>translated</Link> into multiple languages. ' +
+      'Inside it there are tokens <Icon /> which are swapped for ' +
+      'components allowing for dynamic content.';
+
+    this.output =
+      'Hello, this is a <button type="button">test string</button> that can be ' +
+      '<a href="https://capsulecrm.com">translated</a> into multiple languages. ' +
+      'Inside it there are tokens <img src="icon.png" alt="Icon"> which are swapped for ' +
+      'components allowing for dynamic content.';
+
     await render(hbs`
-      <Intl @string={{this.string}} as |intl|>
+      <Intl @string={{this.input}} as |intl|>
         <intl.Button as |string|>
           <button type="button">{{string}}</button>
         </intl.Button>
@@ -33,29 +36,11 @@ module('Integration | Component | intl', function (hooks) {
     `);
 
     assert.strictEqual(
-      this.element.innerHTML.trim(),
-      'Hello, this is a \n' +
-        '        \n' +
-        '          <button type="button">test string</button>\n' +
-        '        \n' +
-        '        <!---->\n' +
-        '        <!---->\n' +
-        '       that can be\n' +
-        '      \n' +
-        '        <!---->\n' +
-        '        \n' +
-        '          <a href="https://capsulecrm.com">translated</a>\n' +
-        '        \n' +
-        '        <!---->\n' +
-        '       into multiple languages.\n' +
-        '      Inside it there are tokens \n' +
-        '        <!---->\n' +
-        '        <!---->\n' +
-        '        \n' +
-        '          <img src="icon.png" alt="Icon">\n' +
-        '        \n' +
-        '      , which are swapped for\n' +
-        '      components allowing for dynamic content.'
+      this.element.innerHTML
+        .trim()
+        .replace(/<!---->/g, '')
+        .replace(/\s+/g, ' '),
+      this.output
     );
   });
 });
