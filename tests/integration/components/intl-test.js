c9880781941d3f2a52919c06c277b1f7ef159cba
Josh Jackson <joshcharlesjackson@gmail.com>
2023-04-17 09:33:12 +0100

    fixup! Update regex to match <Test/> as well as <Test />

--- tests/integration/components/intl-test.js
+++ tests/integration/components/intl-test.js
@@ -12,13 +12,13 @@ module('Integration | Component | intl', function (hooks) {
     this.input =
       'Hello, this is a <Button>test string</Button> that can be ' +
       '<Link>translated</Link> into multiple languages. ' +
-      'Inside it there are tokens <Icon /> which are swapped for ' +
+      'Inside it there are tokens such as <Icon />  or <Tag/> which are swapped for ' +
       'components allowing for dynamic content.';
 
     this.output =
       'Hello, this is a <button type="button">test string</button> that can be ' +
       '<a href="https://capsulecrm.com">translated</a> into multiple languages. ' +
-      'Inside it there are tokens <img src="icon.png" alt="Icon"> which are swapped for ' +
+      'Inside it there are tokens such as <img src="icon.png" alt="Icon"> or <span>test</span> which are swapped for ' +
       'components allowing for dynamic content.';
 
     await render(hbs`
@@ -32,32 +32,9 @@ module('Integration | Component | intl', function (hooks) {
         <intl.Icon>
           <img src="icon.png" alt="Icon" />
         </intl.Icon>
-      </Intl>
-    `);
-
-    assert.strictEqual(
-      this.element.innerHTML
-        .trim()
-        .replace(/<!---->/g, '')
-        .replace(/\s+/g, ' '),
-      this.output
-    );
-  });
-
-  test('it handles self closing tags without a space (regression)', async function (assert) {
-    assert.expect(1);
-
-    this.input =
-      'This is a test <Icon/> with self closing tags that do not have a space between the name and the closing tag.';
-
-    this.output =
-      'This is a test <img src="icon.png" alt="Icon"> with self closing tags that do not have a space between the name and the closing tag.';
-
-    await render(hbs`
-      <Intl @string={{this.input}} as |intl|>
-        <intl.Icon>
-          <img src="icon.png" alt="Icon" />
-        </intl.Icon>
+        <intl.Tag>
+          <span>test</span>
+        </intl.Tag>
       </Intl>
     `);
 
