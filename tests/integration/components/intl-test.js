5b00c1a10d854e385b2e651804b5697e7e86831b
Andrew Kirwin <github@andrewkirwin.co.uk>
2023-07-03 16:53:02 +0100

    add test cases for unfulfilled components and nested syntax

--- tests/integration/components/intl-test.js
+++ tests/integration/components/intl-test.js
@@ -6,44 +6,75 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | intl', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it works', async function (assert) {
+  hooks.beforeEach(function (assert) {
+    this.assertHTML = () => {
+      assert.strictEqual(
+        this.element.innerHTML
+          .trim()
+          .replace(/<!---->/g, '')
+          .replace(/\s+/g, ' '),
+        this.output
+      );
+    };
+  });
+
+  test('block syntax', async function (assert) {
     assert.expect(1);
 
-    this.input =
-      'Hello, this is a <Button>test string</Button> that can be ' +
-      '<Link>translated</Link> into multiple languages. ' +
-      'Inside it there are tokens such as <Icon /> or <Tag/> which are swapped for ' +
-      'components allowing for dynamic content.';
-
-    this.output =
-      'Hello, this is a <button type="button">test string</button> that can be ' +
-      '<a href="https://capsulecrm.com">translated</a> into multiple languages. ' +
-      'Inside it there are tokens such as <img src="icon.png" alt="Icon"> or <span>test</span> which are swapped for ' +
-      'components allowing for dynamic content.';
+    this.input = 'Get <Link>support</Link>';
+    this.output = 'Get <a href="https://capsulecrm.com/support">support</a>';
 
     await render(hbs`
       <Intl @string={{this.input}} as |intl|>
-        <intl.Button as |string|>
-          <button type="button">{{string}}</button>
-        </intl.Button>
         <intl.Link as |string|>
-          <a href="https://capsulecrm.com">{{string}}</a>
+          <a href="https://capsulecrm.com/support">{{string}}</a>
         </intl.Link>
-        <intl.Icon>
-          <img src="icon.png" alt="Icon" />
-        </intl.Icon>
-        <intl.Tag>
-          <span>test</span>
-        </intl.Tag>
       </Intl>
     `);
 
-    assert.strictEqual(
-      this.element.innerHTML
-        .trim()
-        .replace(/<!---->/g, '')
-        .replace(/\s+/g, ' '),
-      this.output
-    );
+    this.assertHTML();
+  });
+
+  test('inline syntax', async function (assert) {
+    assert.expect(1);
+
+    this.input = 'Hello <World />';
+    this.output = 'Hello 🌎';
+
+    await render(hbs`
+      <Intl @string={{this.input}} as |intl|>
+        <intl.World>🌎</intl.World>
+      </Intl>
+    `);
+
+    this.assertHTML();
+  });
+
+  test('nested syntax', async function (assert) {
+    assert.expect(1);
+
+    this.input = 'Visit <Link><Image /> destination</Link>';
+    this.output = 'Visit &lt;Image /&gt; destination';
+
+    await render(hbs`
+      <Intl @string={{this.input}} as |intl|>
+        <intl.Link as |string|>
+          {{string}}
+        </intl.Link>
+      </Intl>
+    `);
+
+    this.assertHTML();
+  });
+
+  test('missing components', async function (assert) {
+    assert.expect(1);
+
+    this.input = 'Hello <World />';
+    this.output = 'Hello ';
+
+    await render(hbs`<Intl @string={{this.input}} />`);
+
+    this.assertHTML();
   });
 });
